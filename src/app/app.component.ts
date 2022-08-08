8e35185f51e1bcd311bc6eca22a258b5d258934a
alishba2 <alishbashazadi013@gmail.com>
2022-08-08 14:39:42 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CalularoApp';
+  title = 'CalulatorApp';
+
+  // public num1:any;
+  // public num2 : any;
+  // public num3: any;
+  // add(){
+  //   this.num3 = this.num1 + this.num2;
+  // }
+  // sub(){
+  //   this.num3 = this.num1 - this.num2;
+  // }
+  // mul(){
+  //   this.num3 = this.num1 * this.num2;
+  // }
+  // divide(){
+  //   this.num3 = this.num1 / this.num2;
+  // }
+  toShow = '0'
+  currValue = ''
+  
+  writetoinput(value:string){
+    this.currValue = this.currValue + value;
+    this.toShow = this.currValue;
+  }
+  equals(){
+    this.toShow = eval(this.currValue);
+  }
+  clear(){
+    this.currValue= '';
+    this.toShow = this.currValue;
+  }
 }
